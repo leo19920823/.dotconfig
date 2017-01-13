da0775080920fdd9131a4c1eeff27ec55e2a38f5
Leo <leo19920823@gmail.com>
2017-01-13 23:12:38 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -3,4 +3,7 @@ apt-get -y upgrade
 apt-get update
 apt-get -y install zsh emacs git vim chromium-browser virtualbox vagrant curl wget
 git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
+cd /usr/src
+wget https://raw.githubusercontent.com/getlantern/lantern-binaries/master/lantern-installer-beta-64-bit.deb
+dpkg -i lantern-installer-beta-64-bit.deb
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
